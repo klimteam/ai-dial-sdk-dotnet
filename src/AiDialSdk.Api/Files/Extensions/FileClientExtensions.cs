0ee80018ecabefc11e5c444f5a087ed64aff9eae
Dzianis K. <10210072+klimteam@users.noreply.github.com>
2026-05-07 20:08:59 +0400

    Add GetDataAsync method to IDialFileApiClient and implement GetDataAsStringAsync extension method

--- src/AiDialSdk.Api/Files/Extensions/FileClientExtensions.cs
+++ src/AiDialSdk.Api/Files/Extensions/FileClientExtensions.cs
@@ -5,6 +5,14 @@ namespace AiDialSdk.Api.Files.Extensions;
 
 public static class FileClientExtensions
 {
+    public static async Task<string> GetDataAsStringAsync(this IDialFileApiClient dialFileApiClient, string filaPath,
+        CancellationToken token)
+    {
+        var stream = await dialFileApiClient.GetDataAsync(filaPath, token);
+        using var reader = new StreamReader(stream);
+        return await reader.ReadToEndAsync(token);
+    }
+    
     public static async Task<PutFileResponse> StoreDataAsJsonAsync<TData>(this IDialFileApiClient dialFileApiClient, 
         string fileName, TData data, JsonSerializerOptions jsonSerializerOptions, CancellationToken token)
     {
